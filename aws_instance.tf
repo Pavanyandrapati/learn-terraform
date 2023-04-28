1d02b5397cdb969211bf11beea4d5fd047a400bf
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-04-28 23:26:44 +0530

    frontend

--- aws_instance.tf
+++ aws_instance.tf
@@ -6,3 +6,19 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+data "aws_security_group" "allow-all" {
+  Name = allow-all
+}
+
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z08045122E2EQN1OR1WS6"
+  name    = "frontend-dev.pavan345.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.public_ip]
+}
